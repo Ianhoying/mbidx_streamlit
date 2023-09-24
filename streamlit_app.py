ea40ef415ae5826296292f9d42f796665a1e1640
Ianhoying <145854610+Ianhoying@users.noreply.github.com>
2023-09-24 09:52:28 +0900

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### 스트림릿 설치
+
+# In[2]:
+
+
+pip install streamlit
+
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt, pandas as pd, streamlit as st
+
+
+# In[3]:
+
+
+st.text("3분기 자기개발 - 스트림릿 구현")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
